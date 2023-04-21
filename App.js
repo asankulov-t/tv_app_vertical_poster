68c0cfcb1e77fc46a4c5055ff78417e97c1adb5b
unknown <asankulov-t>
2023-04-22 01:25:00 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    Dimensions, Image,
+
+} from "react-native";
+import TimedSlideshow from "react-native-timed-slideshow";
+
+
+const { width, heigth } = Dimensions.get("window");
+let flatList;
+
+const App=()=>{
+    let [movies, setMovies] = useState();
+    const url = "https://64411cb4792fe886a89eaf4b.mockapi.io/tv_data";
+    const fetchPopular = async () => {
+        const data = await fetch(url);
+        const res = await data.json();
+        setMovies(res)
+        console.log(res)
+    };
+
+
+
+    let resultOfimg=movies&&movies.map((t)=>{
+        return t.uri
+    })
+    console.log(resultOfimg)
+    useEffect(()=>{
+       fetchPopular()
+    },[])
+    return(
+        movies&&<TimedSlideshow textStyle={{height:"250px"}} items={movies}/>
+
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = {
+    slider: {
+        backgroundColor: '#fff',
+        overflow: 'hidden',
+        width: '100%',
+        height: "100%",
+    },
+    slide: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+    },
+    text: {
+        color: 'white',
+        fontSize: 30,
+    },
+}
+
+
+export default App;
+
+
+
+
+
